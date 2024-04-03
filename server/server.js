f1c12e453def64998063f346adb386a77798567a
bijankhazaei <bijankhazaei@gmail.com>
2024-04-03 13:55:00 +0330

    some changes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,11 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 server.listen(3000, () => {
     console.log('listening on *:3000');
 });
